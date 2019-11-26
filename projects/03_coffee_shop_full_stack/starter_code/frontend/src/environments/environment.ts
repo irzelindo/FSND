70de038f7e9714f625c5011425780a40648dfc66
irzelindo salvador <irzelindo.salvador@gmail.com>
2019-11-26 18:11:07 +0200

    Add login handler on the user page

--- projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
+    url: 'irzelindo.auth0.com', // the auth0 domain prefix
+    audience: 'coffeeshop', // the audience set for the auth0 app
+    clientId: 'UeA0xQQN55lIuSz5MH2jpxxWR5R6AmUI', // the client id generated for the auth0 app
+    callbackURL: 'http://localhost:8100', // the base url of the running ionic application.
   }
 };
